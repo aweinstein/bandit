8778506caf47afa879d29ceefeae9b161e9f3112
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2015-08-10 06:28:26 -0300

    Fix missing alpha bug.

--- bandit.py
+++ bandit.py
@@ -24,16 +24,8 @@ class Agent(object):
         self.bandit = bandit
         n =  bandit.n
         self.n = n
-        if Q_init:
-            self.Q = Q_init * np.ones(n)
-        else:
-            self.Q = np.random.uniform(0, 1e-4, n) # init with small numbers to
-                                                   # avoid ties
-        if alpha:
-            self.update_action_value = self.update_action_value_constant_alpha
-            self.alpha = alpha
-        else:
-            self.update_action_value = self.update_action_value_sample_average
+        self.Q_init = Q_init
+        self.alpha = alpha
         self.reset()
         
     def run(self):
@@ -50,9 +42,9 @@ class Agent(object):
         # Keep track of performance
         self.rewards_seq.append(reward)
         self.actions.append(action)
-        self.average_reward += (reward - self.average_reward) / self.k_reward
         self.k_reward += 1
-        self.optimal_actions.append(action == self.bandit.optimal_action())
+        correct = (action == self.bandit.optimal_action()) * 100
+        self.optimal_actions.append(correct)
 
     def update_action_value_sample_average(self, action, reward):
         k = self.k_actions[action]
@@ -63,6 +55,15 @@ class Agent(object):
         self.Q[action] += self.alpha * (reward - self.Q[action])
 
     def reset(self):
+        if self.Q_init:
+            self.Q = self.Q_init * np.ones(self.n)
+        else: # init with small random numbers to avoid ties
+            self.Q = np.random.uniform(0, 1e-4, self.n)
+        if self.alpha:
+            self.update_action_value = self.update_action_value_constant_alpha
+        else:
+            self.update_action_value = self.update_action_value_sample_average
+
         self.rewards = []
         self.rewards_seq = []
         self.actions = []
@@ -100,7 +101,7 @@ def run_experiment(n_bandits, steps, epsilon, Q_init=None, alpha=None):
     percentage_optimals = np.zeros((n_bandits, steps))
     for i in range(n_bandits):
         bandit = Bandit()
-        agent = Agent(bandit, epsilon, Q_init)
+        agent = Agent(bandit, epsilon, Q_init, alpha)
         for j in range(steps):
             agent.run()
         average_rewards[i,:] = agent.rewards_seq
@@ -138,23 +139,29 @@ def figure_2_4():
     '''Replicate figure 2.4 of Sutton and Barto's book.'''
     np.random.seed(1234)
     epsilons = (0.1, 0)
-    q_inits = (False, 5)
+    q_inits = (0, 5)
     ars, pos = [], []
     for epsilon, q_init in zip(epsilons, q_inits):
-        ar, po = run_experiment(100, 8000, epsilon, q_init, alpha=0.1)
+        ar, po = run_experiment(2000, 1000, epsilon, q_init, alpha=0.1)
         ars.append(np.mean(ar, 0))
         pos.append(np.mean(po, 0))
         
     # plot the results
     plt.close('all')
-    plt.plot(pos[0], label='realistic $\epsilon$-greedy')
-    plt.plot(pos[1], label='optimistic greedy')
-    plt.legend(loc='lower right')
-    plt.xlabel('Plays')
-    plt.ylabel('% Optimal action')
-    plt.xlim(xmin=-20)
+    f, (ax1, ax2) = plt.subplots(2)
+    labels = ('$\epsilon$-greedy', 'optimistic')
+    for i,label in enumerate(labels):
+        ax1.plot(ars[i].T, label=label)
+        ax2.plot(pos[i].T, label=label)
+    ax1.legend(loc='lower right')
+    ax1.set_ylabel('Average reward')
+    ax1.set_xlim(xmin=-10)
+    ax2.legend(loc='lower right')
+    ax2.set_xlabel('Plays')
+    ax2.set_ylabel('% Optimal action')
+    ax2.set_xlim(xmin=-20)
     plt.savefig('fig_2_4.pdf')
     plt.show()
 
 if __name__ == '__main__':
-    figure_2_1()
+    figure_2_4()
