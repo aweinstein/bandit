7834e117c1f5688cd6e345c1c1150d80ffbccab3
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2015-10-16 16:00:20 -0300

    Plot all the cues.

--- vis.py
+++ vis.py
@@ -3,17 +3,18 @@ import seaborn as sns
 import pandas as pd
 
 
-d_map = {3:1, 8:2, 14:3, 23:4}
-df = pd.read_pickle('all_data.pkl')
-df['choices'] = df['choices'].apply(lambda x: d_map[x])
-df = df.loc[df['cues'] == 1]
-df.reset_index(inplace=True)
+if __name__ == '__main__':
+    d_map = {3:1, 8:2, 14:3, 23:4}
+    df = pd.read_pickle('all_data.pkl')
+    df['choices'] = df['choices'].apply(lambda x: d_map[x])
+    df.reset_index(inplace=True)
 
-plt.close('all')
-g = sns.FacetGrid(df, col='subject', col_wrap=6, size=1.5, ylim=(0, 5), aspect=1.5)
-g.map(plt.plot, 'choices')
-g.set(xticks=[], yticks=[1,2,3,4], yticklabels=['3', '8', '14', '23'])
-g.set_ylabels('choice')
-g.fig.tight_layout()
-plt.savefig('actions.pdf')
-plt.show()
+    plt.close('all')
+    g = sns.FacetGrid(df, col='subject', hue='cues',
+                      col_wrap=6, size=1.5, ylim=(0, 5), aspect=1.5)
+    g.map(plt.plot, 'choices')
+    g.set(xticks=[], yticks=[1,2,3,4], yticklabels=['3', '8', '14', '23'])
+    g.set_ylabels('choice')
+    g.fig.tight_layout()
+    plt.savefig('actions.pdf')
+    plt.show()
