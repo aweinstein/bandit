4eb41a5015b176df5a45a2f6825c38dcc17323de
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2015-10-15 21:33:55 -0300

    Add function to make a single dataframe with the trials of all the subjects.

--- parse.py
+++ parse.py
@@ -5,8 +5,13 @@ import os
 import pandas as pd
 from scipy.io import loadmat
 
+DF_Dir = 'data_behavior'
 
-if __name__ == '__main__':
+def matlab_to_dataframe():
+    """Extract behavioral data from mat files and create a dataframe.
+
+    One dataframe per subject is created. The dataframe is pickled and saved.
+    """
     dir_name = 'data'
     for fname in os.listdir(dir_name):
         if fname.endswith('.mat'):
@@ -17,6 +22,17 @@ if __name__ == '__main__':
             for column in columns:
                 data[column] = data[column].flatten()
             df = pd.DataFrame(data, columns=columns)
-            df_file = os.path.join('data_behavior', s + '.pkl')
+            df.index.name = 'trial'
+            df_file = os.path.join(DF_Dir, s + '.pkl')
             print('DataFrame saved in', df_file)
             df.to_pickle(df_file)
+
+def concat_all_dataframes():
+    pkls = os.listdir(DF_Dir)
+    pkls.sort()
+    subjects = [int(s[:2]) for s in pkls]
+    dfs = (pd.read_pickle(os.path.join(DF_Dir,fn)) for fn in pkls)
+    df_all = pd.concat(dfs, keys=subjects)
+    df_all.index.set_names('subject', 0, inplace=True)
+    df_all.to_pickle('all_data.pkl')
+
