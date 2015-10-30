4f8440ee7dcc86ca3a2a5ff21494891888129f62
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2015-10-30 11:13:23 -0300

    Add option to plot_actions.

--- vis.py
+++ vis.py
@@ -3,18 +3,35 @@ import seaborn as sns
 import pandas as pd
 
 
-if __name__ == '__main__':
+def plot_actions(cue=1):
     d_map = {3:1, 8:2, 14:3, 23:4}
     df = pd.read_pickle('all_data.pkl')
     df['choices'] = df['choices'].apply(lambda x: d_map[x])
+    if cue in [1, 2, 3]:
+        df = df.loc[df['cues'] == cue]
     df.reset_index(inplace=True)
 
     plt.close('all')
-    g = sns.FacetGrid(df, col='subject', hue='cues',
-                      col_wrap=6, size=1.5, ylim=(0, 5), aspect=1.5)
+    if cue in [1, 2, 3]:
+        g = sns.FacetGrid(df, col='subject',
+                          col_wrap=6, size=1.5, ylim=(0, 5), aspect=1.5)
+        title = 'Cue {:d}'.format(cue)
+    else:
+        g = sns.FacetGrid(df, col='subject', hue='cues',
+                          col_wrap=6, size=1.5, ylim=(0, 5), aspect=1.5)
+        title = 'All cues'
+
     g.map(plt.plot, 'choices')
     g.set(xticks=[], yticks=[1,2,3,4], yticklabels=['3', '8', '14', '23'])
     g.set_ylabels('choice')
     g.fig.tight_layout()
-    plt.savefig('actions.pdf')
+    g.fig.subplots_adjust(top=0.93)
+    g.fig.suptitle(title)
+    
+    if cue in [1, 2, 3]:
+        fn = 'actions_{:d}.pdf'.format(cue)
+    else:
+        fn = 'actions_all.pdf'
+    plt.savefig(fn)
     plt.show()
+
