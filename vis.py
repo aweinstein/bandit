dad6099c4adf7603249875e22cbdcdfa87de365e
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2015-10-30 16:27:44 -0300

    Add reward to actions visualization.

--- vis.py
+++ vis.py
@@ -10,6 +10,7 @@ def plot_actions(cue=1):
         df = df.loc[df['cues'] == cue]
     df.reset_index(inplace=True)
 
+
     plt.close('all')
     if cue in [1, 2, 3]:
         g = sns.FacetGrid(df, col='subject',
@@ -26,6 +27,20 @@ def plot_actions(cue=1):
     g.fig.tight_layout()
     g.fig.subplots_adjust(top=0.93)
     g.fig.suptitle(title)
+
+    if cue in [1, 2, 3]:
+        subjects = df['subject'].unique()
+        for ax, subject in zip(g.axes, subjects):
+            df_subject = df.loc[df['subject'] == subject]
+            df_subject.reset_index(inplace=True)
+            df_wins = df_subject.loc[df_subject['rewards'] > 0]
+            df_lose = df_subject.loc[df_subject['rewards'] < 0]
+            pos_win = df_wins.loc[df_wins['subject'] == subject].index
+            pos_lose = df_lose.loc[df_lose['subject'] == subject].index
+            ax.eventplot(pos_win, lineoffsets=4.5, linelength=0.75,
+                         linewidths=0.4)
+            ax.eventplot(pos_lose, lineoffsets=4.5, linelength=0.75,
+                         color='r', linewidths=0.4)
     
     if cue in [1, 2, 3]:
         fn = 'actions_{:d}.pdf'.format(cue)
@@ -33,6 +48,7 @@ def plot_actions(cue=1):
         fn = 'actions_all.pdf'
     plt.savefig(fn)
     plt.show()
+    globals().update(locals())
 
 def plot_optimum():
     df = pd.read_pickle('df_n_optimum.pkl')
@@ -40,3 +56,6 @@ def plot_optimum():
     sns.factorplot(data=df, x='block', y='n_optimum', hue='learner', aspect=1.5)
     plt.savefig('n_optimum.pdf')
     plt.show()
+
+if __name__ == '__main__':
+    plot_actions()
