0e145e31bee5321c2f0b3c54a284c726ee2ca830
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2015-11-11 13:41:04 -0300

    Add models for the cue bandit.

--- models.py
+++ models.py
@@ -1,16 +1,25 @@
+from collections import defaultdict
+from itertools import product
+
 import numpy as np
 
 from utils import softmax
 
 class Bandit(object):
+    """Bandit used in [1] as an example.
+
+    [1] N. D. Daw, "Trial-by-trial data analysis using computational models,"
+    Decision making, affect, and learning: Attention and performance XXIII,
+    vol. 23, p. 1, 2011.
+    """
     def __init__(self):
         self.n = 2
 
     def reward(self, action):
         """Return reward given the action.
 
-        Action 0 has probability 0.8 of winning 1.
-        Action 1 has probability 0.2 of winning 0.
+            Action 0 has probability 0.8 of winning 1.
+            Action 1 has probability 0.2 of winning 0.
         """
         probabilities = (0.8, 0.2)
         p = probabilities[action]
@@ -24,25 +33,14 @@ class Bandit(object):
             r = None
         return r
 
-class BanditCard(object):
-    def __init__(self):
-        self.n = 4
-
-    def reward(self, action):
-        """Return reward given the action."""
-        actions_bet = (3, 8, 14, 23)
-        p_win = 0.8
-        if action >=0 and action < self.n:
-            if np.random.rand() < p_win:
-                r = actions_bet[action]
-            else:
-                r = -actions_bet[action]
-        else:
-            print('Error: action out of range')
-            r = None
-        return r
-        
 class Agent(object):
+    """Agent used in [1] as an example.
+
+    [1] N. D. Daw, "Trial-by-trial data analysis using computational models,"
+    Decision making, affect, and learning: Attention and performance XXIII,
+    vol. 23, p. 1, 2011.
+    """
+
     def __init__(self, bandit, alpha=0.25, beta=1):
         self.bandit = bandit
         self.Q = np.zeros(2)
@@ -61,6 +59,28 @@ class Agent(object):
         self.log['action'].append(action)
         self.log['Q(0)'].append(self.Q[0])
         self.log['Q(1)'].append(self.Q[1])
+
+
+class BanditCard(object):
+    def __init__(self):
+        self.n = 4
+
+    def reward(self, action):
+        """Return reward given the action.
+
+        The bandit has no cues.
+        """
+        actions_bet = (3, 8, 14, 23)
+        p_win = 0.8
+        if action >=0 and action < self.n:
+            if np.random.rand() < p_win:
+                r = actions_bet[action]
+            else:
+                r = -actions_bet[action]
+        else:
+            print('Error: action out of range')
+            r = None
+        return r
 
 class AgentCard(object):
     def __init__(self, bandit, alpha=0.25, beta=1):
@@ -84,3 +104,69 @@ class AgentCard(object):
         self.log['Q(1)'].append(self.Q[1])
         self.log['Q(2)'].append(self.Q[2])
         self.log['Q(3)'].append(self.Q[3])
+
+class BanditCardCues(object):
+    def __init__(self):
+        self.n = 4
+
+    def get_cue(self):
+        self.cue = np.random.choice((0, 1, 2))
+        return self.cue
+        
+    def reward(self, action):
+        """Return reward given the action.
+
+        The bandit has no cues.
+        """
+        actions_bet = (3, 8, 14, 23)
+        p_win = [0.8, 0.5, 0.2][self.cue]
+        if action >=0 and action < self.n:
+            if np.random.rand() < p_win:
+                r = actions_bet[action]
+            else:
+                r = -actions_bet[action]
+        else:
+            print('Error: action out of range')
+            r = None
+        return r
+
+class AgentCardCues(object):
+    def __init__(self, bandit, alpha=0.05, beta=1):
+        self.bandit = bandit
+        # Q-values are stored as Q[cue, action]
+        self.Q = np.zeros((3,4))
+        self.alpha = alpha
+        self.beta = beta
+        self.log = defaultdict(list)
+
+    def run(self):
+        actions = (0, 1, 2, 3)
+        cues = (0, 1, 2)
+        cue = self.bandit.get_cue()
+        p = softmax(self.Q[cue,:], self.beta)
+        action = np.random.choice(actions, p=p)
+        reward = self.bandit.reward(action)
+        self.Q[cue, action] += self.alpha * (reward - self.Q[cue, action])
+        
+        self.log['reward'].append(reward)
+        self.log['action'].append(action)
+        self.log['cue'].append(cue)
+        for cue, action in product(cues, actions):
+            key = 'Q({:d},{:d})'.format(cue, action)
+            self.log[key].append(self.Q[cue, action])
+
+if __name__ == '__main__':
+    import pandas as pd
+    bandit = BanditCardCues()
+    agent = AgentCardCues(bandit, alpha=0.05)
+    trials = 300
+    for _ in range(trials):
+        agent.run()
+
+    columns = ['cue', 'action', 'reward']
+    actions = (0, 1, 2, 3)
+    cues = (0, 1, 2)
+    for cue, action in product(cues, actions):
+        columns.append('Q({:d},{:d})'.format(cue, action))
+    
+    df = pd.DataFrame(agent.log, columns=columns)
